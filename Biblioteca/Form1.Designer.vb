46a2e85a2236db9e45dfd14fe484bb407325c89c
Jeremy Fonseca <fonseca.dev@outlook.com>
2022-11-09 14:17:09 -0800

    feat: initial commit

---
+++ Biblioteca/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.CatalogoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AutorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ReportesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CatalogoAutorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CatalogoEditorialToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LibrosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SalirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditorialToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CatalogoToolStripMenuItem, Me.ReportesToolStripMenuItem, Me.SalirToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'CatalogoToolStripMenuItem
+        '
+        Me.CatalogoToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AutorToolStripMenuItem, Me.LibrosToolStripMenuItem, Me.EditorialToolStripMenuItem})
+        Me.CatalogoToolStripMenuItem.Name = "CatalogoToolStripMenuItem"
+        Me.CatalogoToolStripMenuItem.Size = New System.Drawing.Size(67, 20)
+        Me.CatalogoToolStripMenuItem.Text = "Catalogo"
+        '
+        'AutorToolStripMenuItem
+        '
+        Me.AutorToolStripMenuItem.Name = "AutorToolStripMenuItem"
+        Me.AutorToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.AutorToolStripMenuItem.Text = "Autor"
+        '
+        'ReportesToolStripMenuItem
+        '
+        Me.ReportesToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CatalogoAutorToolStripMenuItem, Me.CatalogoEditorialToolStripMenuItem})
+        Me.ReportesToolStripMenuItem.Name = "ReportesToolStripMenuItem"
+        Me.ReportesToolStripMenuItem.Size = New System.Drawing.Size(65, 20)
+        Me.ReportesToolStripMenuItem.Text = "Reportes"
+        '
+        'CatalogoAutorToolStripMenuItem
+        '
+        Me.CatalogoAutorToolStripMenuItem.Name = "CatalogoAutorToolStripMenuItem"
+        Me.CatalogoAutorToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.CatalogoAutorToolStripMenuItem.Text = "Catalogo autor"
+        '
+        'CatalogoEditorialToolStripMenuItem
+        '
+        Me.CatalogoEditorialToolStripMenuItem.Name = "CatalogoEditorialToolStripMenuItem"
+        Me.CatalogoEditorialToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.CatalogoEditorialToolStripMenuItem.Text = "Catalogo editorial"
+        '
+        'LibrosToolStripMenuItem
+        '
+        Me.LibrosToolStripMenuItem.Name = "LibrosToolStripMenuItem"
+        Me.LibrosToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.LibrosToolStripMenuItem.Text = "Libros"
+        '
+        'SalirToolStripMenuItem
+        '
+        Me.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
+        Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(41, 20)
+        Me.SalirToolStripMenuItem.Text = "Salir"
+        '
+        'EditorialToolStripMenuItem
+        '
+        Me.EditorialToolStripMenuItem.Name = "EditorialToolStripMenuItem"
+        Me.EditorialToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.EditorialToolStripMenuItem.Text = "Editorial"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents CatalogoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AutorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ReportesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CatalogoAutorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LibrosToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CatalogoEditorialToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SalirToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditorialToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ Biblioteca/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub SalirToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SalirToolStripMenuItem.Click
+        End
+    End Sub
+
+    Private Sub AutorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AutorToolStripMenuItem.Click
+
+        ' Abrir la vista:
+        ' <nombre-formulario-autor>.ShowDialog()
+
+    End Sub
+End Class
