997cd2aa22219bbb99990cffdafd626ac7a05804
Jeremy Fonseca <fonseca.dev@outlook.com>
2022-11-21 15:12:04 -0800

    add RepoterEditoriales

---
+++ Biblioteca/RepoterEditoriales.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class RepoterEditoriales
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.ButtonSalir = New System.Windows.Forms.Button()
+        Me.ButtonExport = New System.Windows.Forms.Button()
+        Me.DataGridEditoriales = New System.Windows.Forms.DataGridView()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.DataGridEditoriales, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Location = New System.Drawing.Point(420, 8)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(83, 25)
+        Me.Label1.TabIndex = 30
+        Me.Label1.Text = "Buscar"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(423, 36)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(225, 20)
+        Me.TextBox1.TabIndex = 33
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Transparent
+        Me.GroupBox1.Controls.Add(Me.ButtonSalir)
+        Me.GroupBox1.Controls.Add(Me.ButtonExport)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 8)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(267, 58)
+        Me.GroupBox1.TabIndex = 32
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Opciones"
+        '
+        'ButtonSalir
+        '
+        Me.ButtonSalir.Location = New System.Drawing.Point(6, 16)
+        Me.ButtonSalir.Name = "ButtonSalir"
+        Me.ButtonSalir.Size = New System.Drawing.Size(120, 30)
+        Me.ButtonSalir.TabIndex = 9
+        Me.ButtonSalir.Text = "Salir"
+        Me.ButtonSalir.UseVisualStyleBackColor = True
+        '
+        'ButtonExport
+        '
+        Me.ButtonExport.Location = New System.Drawing.Point(132, 15)
+        Me.ButtonExport.Name = "ButtonExport"
+        Me.ButtonExport.Size = New System.Drawing.Size(120, 30)
+        Me.ButtonExport.TabIndex = 12
+        Me.ButtonExport.Text = "Exportar"
+        Me.ButtonExport.UseVisualStyleBackColor = True
+        '
+        'DataGridEditoriales
+        '
+        Me.DataGridEditoriales.BackgroundColor = System.Drawing.Color.White
+        Me.DataGridEditoriales.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridEditoriales.Location = New System.Drawing.Point(12, 72)
+        Me.DataGridEditoriales.Name = "DataGridEditoriales"
+        Me.DataGridEditoriales.Size = New System.Drawing.Size(636, 276)
+        Me.DataGridEditoriales.TabIndex = 31
+        '
+        'RepoterEditoriales
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Biblioteca.My.Resources.Resources.Grade_Grey
+        Me.ClientSize = New System.Drawing.Size(662, 369)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.DataGridEditoriales)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "RepoterEditoriales"
+        Me.Text = "Repoter Editoriales"
+        Me.GroupBox1.ResumeLayout(False)
+        CType(Me.DataGridEditoriales, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents ButtonSalir As Button
+    Friend WithEvents ButtonExport As Button
+    Friend WithEvents DataGridEditoriales As DataGridView
+End Class
---
+++ Biblioteca/RepoterEditoriales.vb
@@ -0,0 +1,21 @@
+﻿Public Class RepoterEditoriales
+
+    Dim conexion As conexion = New conexion()
+
+    Private Sub ButtonSalir_Click(sender As Object, e As EventArgs) Handles ButtonSalir.Click
+        Me.Close()
+    End Sub
+
+    Private Sub RepoterEditoriales_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        conexion.conectar()
+        RenderData()
+    End Sub
+
+    Public Sub RenderData()
+        Dim sql As String
+        sql = "call seleccionar_editorial()"
+        conexion.consulta(sql)
+        Me.DataGridEditoriales.DataSource = conexion.ds.Tables(0)
+    End Sub
+
+End Class
