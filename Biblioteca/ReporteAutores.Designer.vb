070d545bfc08f4db9fcc0ce274fcb8ab3a8f39e8
Jeremy Fonseca <fonseca.dev@outlook.com>
2022-11-21 15:08:35 -0800

    add reporte autores

---
+++ Biblioteca/ReporteAutores.Designer.vb
@@ -0,0 +1,116 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ReporteAutores
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.ButtonSalir = New System.Windows.Forms.Button()
+        Me.ButtonExport = New System.Windows.Forms.Button()
+        Me.DataGridAutores = New System.Windows.Forms.DataGridView()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.DataGridAutores, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Location = New System.Drawing.Point(420, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(83, 25)
+        Me.Label1.TabIndex = 30
+        Me.Label1.Text = "Buscar"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(423, 42)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(225, 20)
+        Me.TextBox1.TabIndex = 33
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Transparent
+        Me.GroupBox1.Controls.Add(Me.ButtonSalir)
+        Me.GroupBox1.Controls.Add(Me.ButtonExport)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 14)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(276, 58)
+        Me.GroupBox1.TabIndex = 32
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Opciones"
+        '
+        'ButtonSalir
+        '
+        Me.ButtonSalir.Location = New System.Drawing.Point(6, 18)
+        Me.ButtonSalir.Name = "ButtonSalir"
+        Me.ButtonSalir.Size = New System.Drawing.Size(120, 30)
+        Me.ButtonSalir.TabIndex = 9
+        Me.ButtonSalir.Text = "Salir"
+        Me.ButtonSalir.UseVisualStyleBackColor = True
+        '
+        'ButtonExport
+        '
+        Me.ButtonExport.Location = New System.Drawing.Point(132, 18)
+        Me.ButtonExport.Name = "ButtonExport"
+        Me.ButtonExport.Size = New System.Drawing.Size(120, 30)
+        Me.ButtonExport.TabIndex = 12
+        Me.ButtonExport.Text = "Exportar"
+        Me.ButtonExport.UseVisualStyleBackColor = True
+        '
+        'DataGridAutores
+        '
+        Me.DataGridAutores.BackgroundColor = System.Drawing.Color.White
+        Me.DataGridAutores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridAutores.Location = New System.Drawing.Point(12, 78)
+        Me.DataGridAutores.Name = "DataGridAutores"
+        Me.DataGridAutores.Size = New System.Drawing.Size(636, 276)
+        Me.DataGridAutores.TabIndex = 31
+        '
+        'ReporteAutores
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Biblioteca.My.Resources.Resources.Grade_Grey
+        Me.ClientSize = New System.Drawing.Size(660, 375)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.DataGridAutores)
+        Me.Name = "ReporteAutores"
+        Me.Text = "Reporte Autores"
+        Me.GroupBox1.ResumeLayout(False)
+        CType(Me.DataGridAutores, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents ButtonSalir As Button
+    Friend WithEvents ButtonExport As Button
+    Friend WithEvents DataGridAutores As DataGridView
+End Class
---
+++ Biblioteca/ReporteAutores.vb
@@ -0,0 +1,18 @@
+﻿Public Class ReporteAutores
+    Dim conexion As conexion = New conexion()
+    Private Sub ReporteAutores_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        conexion.conectar()
+        RenderData()
+    End Sub
+
+    Public Sub RenderData()
+        Dim sql As String
+        sql = "call seleccionar_autores()"
+        conexion.consulta(sql)
+        Me.DataGridAutores.DataSource = conexion.ds.Tables(0)
+    End Sub
+
+    Private Sub ButtonSalir_Click(sender As Object, e As EventArgs) Handles ButtonSalir.Click
+        Me.Close()
+    End Sub
+End Class
