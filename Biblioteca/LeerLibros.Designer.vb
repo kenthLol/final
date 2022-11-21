594def4a16ef94d978430f9bc8d575289ef0460c
Jeremy Fonseca <fonseca.dev@outlook.com>
2022-11-21 15:06:47 -0800

    add leer libros

---
+++ Biblioteca/LeerLibros.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class LeerLibros
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.DataGridLibros = New System.Windows.Forms.DataGridView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.DataGridLibros, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Location = New System.Drawing.Point(9, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(83, 25)
+        Me.Label1.TabIndex = 30
+        Me.Label1.Text = "Buscar"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 42)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(225, 20)
+        Me.TextBox1.TabIndex = 32
+        '
+        'DataGridLibros
+        '
+        Me.DataGridLibros.BackgroundColor = System.Drawing.Color.White
+        Me.DataGridLibros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridLibros.Location = New System.Drawing.Point(12, 78)
+        Me.DataGridLibros.Name = "DataGridLibros"
+        Me.DataGridLibros.Size = New System.Drawing.Size(840, 305)
+        Me.DataGridLibros.TabIndex = 31
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 403)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(141, 32)
+        Me.Button1.TabIndex = 33
+        Me.Button1.Text = "Iniciar Sesión"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(159, 403)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(141, 32)
+        Me.Button2.TabIndex = 34
+        Me.Button2.Text = "Cerrar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'LeerLibros
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(878, 449)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.DataGridLibros)
+        Me.Name = "LeerLibros"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "LeerLibros"
+        CType(Me.DataGridLibros, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents DataGridLibros As DataGridView
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Biblioteca/LeerLibros.vb
@@ -0,0 +1,26 @@
+﻿Public Class LeerLibros
+
+    Dim conexion As conexion = New conexion()
+
+    Private Sub LeerLibros_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        conexion.conectar()
+        RenderData()
+
+    End Sub
+
+    Public Sub RenderData()
+        Dim sql As String
+        sql = "call seleccionar_libros()"
+        conexion.consulta(sql)
+        Me.DataGridLibros.DataSource = conexion.ds.Tables(0)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+        Login.ShowDialog()
+    End Sub
+End Class
